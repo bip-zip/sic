d3b5cf8b46192ac5de35c6cb02f9c5742a061ecd
bip-zip <bipindhakal44@gmail.com>
2024-05-24 13:14:29 +0545

    add separate urls and template folders

--- info/urls.py
+++ info/urls.py
@@ -3,10 +3,10 @@ from .views import *
 
 app_name = 'info'
 
+
 urlpatterns = [
     path('',homeview, name='home' ),
     path('contact-us',contactview, name='contact' ),
     path('notices',noticeview, name='notices' ),
-    
 ]
 
