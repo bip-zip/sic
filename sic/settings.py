8aa5c0a4712666c2752992635c610d0ddcce5289
bip-zip <bipindhakal44@gmail.com>
2024-05-27 15:19:52 +0545

    add database conf

--- sic/settings.py
+++ sic/settings.py
@@ -70,10 +70,23 @@ WSGI_APPLICATION = 'sic.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+'default': {
+
+'ENGINE': 'django.db.backends.mysql',
+
+'NAME': 'sic',
+
+'USER': 'root',
+
+'PASSWORD': '',
+
+'HOST': 'localhost',
+
+'PORT': '3306',
+
+}
+
 }
 
 
